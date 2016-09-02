59ee60400eb621ab1341f95550af8cf6ee4fd024
treter32 <shippedtonowhere@gmail.com>
2016-09-02 11:06:14 -0700

    Added Files

---
+++ APLesson_01/lab01Cat.java
@@ -0,0 +1,18 @@
+public class lab01Cat
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" _");
+		System.out.println("(  \\");
+		System.out.println(" )  )    ...");
+		System.out.println("(  )   .-   -. A.-.A");
+		System.out.println(" \\  \\/      \\/    '  '   \\");
+		System.out.println("  \\    \\     =;   t   /=");
+		System.out.println("   \\     |''''''.  ',---'");
+		System.out.println("   / //   |  ||");
+		System.out.println(" _,))    |_,))");
+		
+		
+		
+	}
+}
---
+++ APLesson_01/lab01Owl.java
@@ -0,0 +1,16 @@
+public class lab01Owl
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello Kappa");
+		
+		System.out.println("Hello Kappa Again");
+		System.out.println("\r");
+		System.out.println("\r");
+		
+		System.out.println("  ____");
+		System.out.println(" (@.@)");
+		System.out.println("-/),,,,)-");
+		System.out.println("  v    v");	
+	}
+}
