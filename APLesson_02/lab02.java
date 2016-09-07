208f9ef141f06e03787f5e8384432634f5943b99
treter32 <shippedtonowhere@gmail.com>
2016-09-07 11:29:36 -0700

    Lab 2

---
+++ APLesson_02/lab02.java
@@ -0,0 +1,30 @@
+public class lab02
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		int z = 6;
+		int sa = 2*x*y+2*x*z+2*y*z;
+		int result = y * x;
+		String name = "Tony Yu";
+		String address = "123 Egypt Lane 123";
+		String city = "City of Egypt";
+		String zip = "92130";
+		
+		System.out.println("The answer is " + result);
+		System.out.println("\r");
+		System.out.println("\r");
+		System.out.println("\r");
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(city);
+		System.out.println(zip);
+		System.out.println("\r");
+		System.out.println("\r");
+		System.out.println("\r");
+		System.out.println(sa);
+	
+	}	
+	
+}
