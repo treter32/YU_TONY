673e37025665670b0618e897cadb22e7842be5c8
treter32 <shippedtonowhere@gmail.com>
2016-09-15 10:55:07 -0700

    hehe xd

--- APLesson_03/RudeAI.java
+++ APLesson_03/RudeAI.java
@@ -7,27 +7,27 @@ public class RudeAI
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("What is your name?");
-		String name = keyboard.next();
+		String name = keyboard.nextLine();
 		System.out.println(name + "? Trash name.");
 		
 		System.out.println("How old are you?");
-		String age = keyboard.next();
+		String age = keyboard.nextLine();
 		System.out.println(age + "? That's the worst time of your life.");
 		
 		System.out.println("What do you do for fun?");
-		String hobby = keyboard.next();
+		String hobby = keyboard.nextLine();
 		System.out.println("Haha," + hobby + " is for losers.");
 		
 		System.out.println("What kind of music do you like?");
-		String music = keyboard.next();
+		String music = keyboard.nextLine();
 		System.out.println(music + " is the worst kind of music.");
 		
 		System.out.println("How many siblings do you have?");
-		String siblings = keyboard.next();
+		String siblings = keyboard.nextLine();
 		System.out.println(siblings +"? Well I bet it sucks to be your sibling.");
 		
 		System.out.println("What do you want to be when you grow up?");
-		String dream = keyboard.next();
+		String dream = keyboard.nextLine();
 		System.out.println(dream + "? Get a life.");
 		
 		
