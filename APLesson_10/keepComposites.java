dec68205eba30498cbd13d4c82b951b7dfdf8b6b
treter32 <shippedtonowhere@gmail.com>
2017-01-06 10:37:31 -0800

    fghjkl,;

---
+++ APLesson_10/keepComposites.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class keepComposites
+{	   
+	public static void main(String[]args)
+	{
+		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
+		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
+		System.out.println(nums);
+	}
+	
+	public static void gFactor(number n)
+	{
+		for(int i = 2; i <= n; i++)
+		{
+			
+				
+		}
+	}
+	
+
+}
