1b759978455120f17d640f3b7dbcbc0071a54d6d
treter32 <shippedtonowhere@gmail.com>
2017-05-17 12:01:25 -0700

    asd
    asasd

---
+++ pogchamp/New Unity Project 1/Assets/Parser/dialogueManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class dialogueManager : MonoBehaviour 
+{
+	dialogueScript parser;
+
+	public string dialogue, characterName;
+	public int lineNum;
+	int pose;
+	string position;
+	string[] options;
+	public bool playerTalking;
+	List<Button> buttons = new List<Button> ();
+
+	public Text dialogueBox;
+	public Text nameBox;
+	public GameObject choiceBox;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
