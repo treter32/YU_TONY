b753878f36fab3ce3ed4a773b7467e3c3c6595d0
treter32 <shippedtonowhere@gmail.com>
2017-05-25 12:49:20 -0700

    dsad
    sad

---
+++ pogchamp/New Unity Project 1/Assets/Text/dialogueScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class dialogueScript : MonoBehaviour 
+{
+	Text text;
+	public TextAsset asset;
+	void Start() 
+	{
+		print (asset.text);
+	}
+
+	void Awake()
+	{
+		text = GetComponent <Text> ();
+	}
+
+	void Update()
+	{
+		text.text = asset.text;
+	}
+}
